fd0949597932eafbed4bd6f5bd84e4830db31b59
Junaid Inam <junaidinam@live.com>
2018-03-29 16:39:52 -0400

    first navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component,AfterViewInit } from '@angular/core';
+import * as Parallax from 'parallax-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit{
+    junaidImagePath: string;
+    purpleLightsImagePath: string;
+    blueLightsImagePath: string;
+
+    logoImagePath: string;
+
+    constructor(){
+      this.junaidImagePath = '/assets/junaid2.png';
+      this.purpleLightsImagePath = '/assets/purple.png';
+      this.blueLightsImagePath = '/assets/blue.png';
+      this.logoImagePath = '/assets/4.png';
+    }
+
+  ngAfterViewInit(){
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+  }
 }
